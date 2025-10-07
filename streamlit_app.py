0fa81545c63a19db96fb151fc88ed1809123c1c7
Rudenko Evgeniy <65402758+M1nX2@users.noreply.github.com>
2025-10-07 19:31:33 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
+titanic = pd.read_csv('https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv', index_col='PassengerId')
+
+def sibsp_group(n):
+    if n == 0:
+        return '0'
+    elif 1 <= n <= 2:
+        return '1–2'
+    else:
+        return '>2'
+
+titanic['SibSp_group'] = titanic['SibSp'].apply(sibsp_group)
+
+survival_share = titanic.groupby(['Sex', 'SibSp_group'])['Survived'].mean().reset_index()
+
+survival_share['Survived (%)'] = (survival_share['Survived'] * 100).round(1)
+
+
+st.title("Лаб3 в5 Руденко")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    survival_share
 )
