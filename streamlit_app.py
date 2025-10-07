e66a19e6f91c4173e8e32feeae3a16839ecec5b8
Rudenko Evgeniy <65402758+M1nX2@users.noreply.github.com>
2025-10-07 19:36:30 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
-import streamlit as st
 import pandas as pd
+import ipywidgets as widgets
+from IPython.display import display
 
-titanic = pd.read_csv('https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv', index_col='PassengerId')
+titanic = pd.read_csv(
+    'https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv',
+    index_col='PassengerId'
+)
 
 def sibsp_group(n):
     if n == 0:
@@ -14,11 +18,19 @@ def sibsp_group(n):
 titanic['SibSp_group'] = titanic['SibSp'].apply(sibsp_group)
 
 survival_share = titanic.groupby(['Sex', 'SibSp_group'])['Survived'].mean().reset_index()
-
 survival_share['Survived (%)'] = (survival_share['Survived'] * 100).round(1)
 
+dropdown = widgets.Dropdown(
+    options=['0', '1–2', '>2'],
+    value='0',
+    description='SibSp:',
+    style={'description_width': '70px'}
+)
+
+
+def update_table(selected_group):
+    filtered = survival_share[survival_share['SibSp_group'] == selected_group]
+    display(filtered)
 
 st.title("Лаб3 в5 Руденко")
-st.write(
-    survival_share
-)
+widgets.interact(update_table, selected_group=dropdown)
