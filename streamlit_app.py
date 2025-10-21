408484544018f38e9c69b088208f6b0a3a8dad31
Rudenko Evgeniy <65402758+M1nX2@users.noreply.github.com>
2025-10-21 19:43:35 +0500

    Refactor survival share calculation and dataset loading

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# Загружаем датасет
-titanic = pd.read_csv(
-    'https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv',
-    index_col='PassengerId'
-)
-
-def sibsp_group(n):
+def sibsp_group(n: int) -> str:
     if n == 0:
         return '0'
     elif 1 <= n <= 2:
@@ -15,15 +9,27 @@ def sibsp_group(n):
     else:
         return '>2'
 
+def calc_survival_share(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.copy()
+    df['SibSp_group'] = df['SibSp'].apply(sibsp_group)
+    
+    survival_share = (
+        df.groupby(['Sex', 'SibSp_group'])['Survived']
+        .mean()
+        .reset_index()
+    )
+    survival_share['Survived (%)'] = (survival_share['Survived'] * 100).round(1)
+    return survival_share
+
+# Загружаем датасет
+titanic = pd.read_csv(
+    'https://huggingface.co/datasets/ankislyakov/titanic/resolve/main/titanic_train.csv',
+    index_col='PassengerId'
+)
+
 titanic['SibSp_group'] = titanic['SibSp'].apply(sibsp_group)
 
-survival_share = (
-    titanic
-    .groupby(['Sex', 'SibSp_group'])['Survived']
-    .mean()
-    .reset_index()
-)
-survival_share['Survived (%)'] = (survival_share['Survived'] * 100).round(1)
+survival_share = calc_survival_share(titanic)
 
 st.title("Лаб3(+4) в5 Руденко")
 
